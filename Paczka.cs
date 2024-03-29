c6f1e54dd3ca7b9d3a56351e557ee5b81161728e
warelisStud <patrycja.warelis@edu.uekat.pl>
2024-03-29 19:35:58 +0100

    Dodaj pliki projektów.

---
+++ Paczka.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z1._3
+{
+    internal class Paczka
+    {
+        private string rodzajPaczki;
+        int dlugosc, wysokosc, szerokosc;
+        int objetosc;
+        public static int MaxObj(Paczka[] paczki)
+        {
+            int max = 0;
+            foreach(Paczka p in paczki)
+            {
+                if(p.objetosc > max)
+                {
+                    max = p.objetosc;
+                }
+            }
+            return max;
+        }
+        public Paczka(string rodzajPaczki, int dlugosc, int wysokosc, int szerokosc)
+        {
+            this.rodzajPaczki = rodzajPaczki;
+            this.dlugosc = dlugosc;
+            this.wysokosc = wysokosc;
+            this.szerokosc = szerokosc;
+            objetosc = dlugosc * wysokosc * szerokosc;
+        }
+
+        public override string ToString()
+        {
+            return $"Rodzaj paczki: {rodzajPaczki}, długość: {dlugosc}, wysokosc: {wysokosc}, szerokosc: {szerokosc}, objetosc: {objetosc}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Z1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Paczka[] paczki = {new ("ekonomiczna", 4, 5, 6),
+                               new("priorytetowa", 1, 3, 3),
+                               new("ekonomiczna", 5, 5, 5)};
+            foreach (Paczka p in paczki)
+            {
+                Console.WriteLine(p.ToString());
+            }
+            
+            Console.WriteLine(Paczka.MaxObj(paczki));
+            
+        }
+    }
+}
